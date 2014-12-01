9c43e32c19a2e8777eb5832b30b83f9ded5f6417
牛海青 <haiqing1@staff.sina.com.cn>
2014-12-01 15:46:32 +0800

    Follow the document.

--- src/include/protocol.h
+++ src/include/protocol.h
@@ -1,26 +1,27 @@
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
 
-#define	FRAME_FLAG_MASK_PRIO	0x38
+#include <stdint.h>
 
-#define	FRAME_FLAG_MASK_CRYPT	0x07
+typedef uint32_t flowid_t;
+
+struct frame_st { 
+	uint16_t body_len; 
+	uint8_t version;
+	uint8_t frame_flags; 
+	uint8_t frame_body[0]; 
+} __attribute__((packed)); 
+#define	FRAME_CRYPT(X)	(((X)->frame_flags)&FRAME_FLAG_MASK_CRYPT)
+#define	FRAME_FLAG_MASK_CRYPT		0x07
 #define	FRAME_FLAG_NOCRYPT			0
 #define	FRAME_FLAG_CRYPT_PRND		1
 #define	FRAME_FLAG_CRYPT_BLOWFISH	2
 
-struct frame_st {
-    uint16_t body_len;
-    uint8_t version;
-    uint8_t frame_flags;
-    uint8_t frame_body[0];
-};
-#define	FRAME_CRYPT(X)	(((X)->frame_flags)&FRAME_FLAG_MASK_CRYPT)
-
-struct frame_body_data_st {
-	uint32_t msg_type:1;	/* Must == 0 */
-	uint32_t flow_id:31;
-	uint8_t data[0];
-};
+struct frame_body_data_st { 
+	uint32_t msg_type:1;	/* Must == 0 */ 
+	uint32_t flow_id:31; 
+	uint8_t data[0]; 
+} __attribute__((packed)); 
 
 enum opcode_enum {
 	OP_TRUNK_CONFIG		=0x02,
@@ -34,41 +35,42 @@ enum opcode_enum {
 
 struct frame_body_ctl_trunk_config_st {	/* server to client */
 	uint32_t msg_type:1;	/* Must == 1 */
-	uint32_t opcode:31;		/* Must == OP_TRUNKER_CONFIG */
-	uint8_t certificate_of_server[0];
-};
+	uint32_t opcode:31;		/* Must == OP_TRUNK_CONFIG */
+	uint8_t x509_certificate[0];
+} __attribute__((packed));
 
-struct frame_body_ctl_trunk_ok_st {	/* client to server */
+struct frame_body_ctl_trunk_ok_st {		/* client to server */
 	uint32_t msg_type:1;	/* Must == 1 */
 	uint32_t opcode:31;		/* Must == OP_TRUNK_OK */
 	uint8_t encrypted_shared_key[0];
-};
+} __attribute__((packed));
 
 struct frame_body_ctl_trunk_failure_st {
 	uint32_t msg_type:1;	/* Must == 1 */
 	uint32_t opcode:31;		/* Must == OP_TRUNK_FAILURE */
-	uint8_t error_msg[0]
-};
+	uint8_t error_msg[0];
+} __attribute__((packed));
 
 struct frame_body_ctl_flow_open_st {
 	uint32_t msg_type:1;	/* Must == 1 */
 	uint32_t opcode:31;		/* Must == OP_FLOW_OPEN */
-	uint32_t flow_id;
-	uint8_t http_request[0]
-};
+	uint32_t flow_id; 
+	uint16_t max_delay_in_ms;
+	uint8_t request_url[0] ;
+} __attribute__((packed)); 
 
-struct frame_body_ctl_flow_failure_st {
+struct frame_body_ctl_flow_failure_st { 
 	uint32_t msg_type:1;	/* Must == 1 */
 	uint32_t opcode:31;		/* Must == OP_FLOW_FAILURE */
-	uint32_t flow_id;		/* */
-	uint8_t error_msg[0]
-};
-
-struct frame_body_ctl_flow_close_st {
-	uint32_t msg_type:1;	/* Must == 1 */
-	uint32_t opcode:31;		/* Must == OP_FLOW_CLOSE */
 	uint32_t flow_id;
-};
+	uint8_t error_msg[0];
+} __attribute__((packed));
+
+struct frame_body_ctl_flow_close_st { 
+	uint32_t msg_type:1;	/* Must == 1 */
+	uint32_t opcode:31;		/* Must == OP__CLOSE */
+	uint32_t flow_id;
+} __attribute__((packed));
 
 #endif
 
