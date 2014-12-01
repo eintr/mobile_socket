402f8caec29749c15156415c64fbd82909c7f8e5
牛海青 <haiqing1@staff.sina.com.cn>
2014-12-01 12:51:51 +0800

    Adjust message format according the document.

--- src/include/protocol.h
+++ src/include/protocol.h
@@ -4,8 +4,9 @@
 #define	FRAME_FLAG_MASK_PRIO	0x38
 
 #define	FRAME_FLAG_MASK_CRYPT	0x07
-#define	FRAME_FLAG_CRYPT_NULL	0
-#define	FRAME_FLAG_CRYPT_BLOWFISH	1
+#define	FRAME_FLAG_NOCRYPT			0
+#define	FRAME_FLAG_CRYPT_PRND		1
+#define	FRAME_FLAG_CRYPT_BLOWFISH	2
 
 struct frame_st {
     uint16_t body_len;
@@ -14,7 +15,6 @@ struct frame_st {
     uint8_t frame_body[0];
 };
 #define	FRAME_CRYPT(X)	(((X)->frame_flags)&FRAME_FLAG_MASK_CRYPT)
-#define	FRAME_PRIO(X)	((((X)->frame_flags)&FRAME_FLAG_MASK_PRIO)>>3)
 
 struct frame_body_data_st {
 	uint32_t msg_type:1;	/* Must == 0 */
@@ -23,70 +23,52 @@ struct frame_body_data_st {
 };
 
 enum opcode_enum {
-//	OP_TRUNKER_INIT		=0x01,
-	OP_TRUNKER_CONFIG	=0x02,
-	OP_TRUNKER_OK		=0x03,
-	OP_TRUNKER_FAILURE	=0x04,
+	OP_TRUNK_CONFIG		=0x02,
+	OP_TRUNK_OK			=0x03,
+	OP_TRUNK_FAILURE	=0x04,
 
-	OP_FRAMER_OPEN		=0x11,
-	OP_FRAMER_FAILURE	=0x12,
-	OP_FRAMER_CLOSE		=0x13,
+	OP_FLOW_OPEN	=0x11,
+	OP_FLOW_FAILURE	=0x12,
+	OP_FLOW_CLOSE	=0x13,
 };
 
-#if 0
-struct frame_body_ctl_truncer_init_st {	/* Client to server. For load balancer friendly,
-											this frame should NEVER be encrypted! */
-	uint32_t msg_type:1;	/* Must == 1 */
-	uint32_t opcode:31;		/* Must == OP_TRUNKER_INIT */
-	uint32_t client_cookie;		/* 0 means request a new one. */
-	//uint8_t certificate_of_client[0];	/*  */
-};
-#endif
-
-struct frame_body_ctl_truncer_config_st {	/* server to client */
+struct frame_body_ctl_trunk_config_st {	/* server to client */
 	uint32_t msg_type:1;	/* Must == 1 */
 	uint32_t opcode:31;		/* Must == OP_TRUNKER_CONFIG */
 	uint8_t certificate_of_server[0];
 };
 
-struct frame_body_ctl_truncer_ok_st {	/* client to server */
+struct frame_body_ctl_trunk_ok_st {	/* client to server */
 	uint32_t msg_type:1;	/* Must == 1 */
-	uint32_t opcode:31;		/* Must == OP_TRUNKER_OK */
+	uint32_t opcode:31;		/* Must == OP_TRUNK_OK */
 	uint8_t encrypted_shared_key[0];
 };
 
-struct frame_body_ctl_truncer_failure_st {
+struct frame_body_ctl_trunk_failure_st {
 	uint32_t msg_type:1;	/* Must == 1 */
-	uint32_t opcode:31;		/* Must == OP_TRUNKER_FAILURE */
+	uint32_t opcode:31;		/* Must == OP_TRUNK_FAILURE */
 	uint8_t error_msg[0]
 };
 
-struct frame_body_ctl_framer_open_st {
+struct frame_body_ctl_flow_open_st {
 	uint32_t msg_type:1;	/* Must == 1 */
-	uint32_t opcode:31;		/* Must == OP_FRAMER_OPEN */
+	uint32_t opcode:31;		/* Must == OP_FLOW_OPEN */
 	uint32_t flow_id;
 	uint8_t http_request[0]
 };
 
-struct frame_body_ctl_framer_failure_st {
+struct frame_body_ctl_flow_failure_st {
 	uint32_t msg_type:1;	/* Must == 1 */
-	uint32_t opcode:31;		/* Must == OP_FRAMER_FAILURE */
+	uint32_t opcode:31;		/* Must == OP_FLOW_FAILURE */
 	uint32_t flow_id;		/* */
 	uint8_t error_msg[0]
 };
 
-struct frame_body_ctl_framer_close_st {
+struct frame_body_ctl_flow_close_st {
 	uint32_t msg_type:1;	/* Must == 1 */
-	uint32_t opcode:31;		/* Must == OP_FRAMER_CLOSE */
+	uint32_t opcode:31;		/* Must == OP_FLOW_CLOSE */
 	uint32_t flow_id;
 };
-
-
-//struct frame_body_ctl_ack_st {
-//	uint32_t msg_type:1;	/* Must == 1 */
-//	uint32_t opcode:31;		/* Must == OP_ACK */
-//	uint32_t ack_serial;	/* */
-//};
 
 #endif
 
