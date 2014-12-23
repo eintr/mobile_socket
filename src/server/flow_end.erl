c337028c81f1a128f6fc32f09acd0cc323de54f7
牛海青 <haiqing1@staff.sina.com.cn>
2014-12-23 18:40:28 +0800

    Expose CryptFlag also.

--- src/server/flow_end.erl
+++ src/server/flow_end.erl
@@ -1,29 +1,31 @@
 -module(flow_end).
 -behaviour(gen_fsm).
 
--export([start_link/3]).
+-export([start_link/5]).
 -export([init/1, handle_info/3, relay/2, terminate/3]).
 
 -import(log, [log/2, log/3]).
 
-start_link(Pid_trunk_end, UpstreamAddr, Flowid) ->
-	gen_fsm:start_link(?MODULE, [Pid_trunk_end, UpstreamAddr, Flowid], []).
+start_link(Pid_trunk_end, UpstreamAddr, Flowid, CryptFlag, Req) ->
+	gen_fsm:start_link(?MODULE, [Pid_trunk_end, UpstreamAddr, Flowid, CryptFlag, Req], []).
 
-init([TrunkEnd, {IP, Port}, FlowID]) ->
-	{ok, Upstream} = gen_tcp:connect(IP, Port, [binary, {active, false}]),
+init([TrunkEnd, {IP, Port}, FlowID, CryptFlag, Req]) ->
+	{ok, Upstream} = gen_tcp:connect(IP, Port, [binary, {active, true}]),
 	log(log_info, "Connected to upstream ~p:~p", [IP, Port]),
-	{ok, relay, {TrunkEnd, Upstream, FlowID, [{upstream, Upstream}]}}.
+	gen_tcp:send(Upstream, Req),
+	log(log_info, "Sent [~p] to upstream.", [Req]),
+	{ok, relay, {TrunkEnd, Upstream, FlowID, CryptFlag, [{upstream, Upstream}]}}.
 
 handle_info(Info, StateName, State) ->
 	?MODULE:StateName(Info, State).
 
-relay({tcp, Upstream, Data}, {TrunkEnd, Upstream, FlowID, _Context}=State) ->
-	TrunkEnd ! {flowdata, FlowID, Data},
+relay({tcp, Upstream, Data}, {TrunkEnd, Upstream, FlowID, CryptFlag, _Context}=State) ->
+	TrunkEnd ! {flowdata, FlowID, CryptFlag, Data},
 	{next_state, relay, State};
-relay({tcp_close, Upstream}, {TrunkEnd, Upstream, FlowID, _Context}=State) ->
-	TrunkEnd ! {flowctl, close, FlowID},
+relay({tcp_closed, Upstream}, {TrunkEnd, Upstream, FlowID, CryptFlag, _Context}=State) ->
+	TrunkEnd ! {flowctl, close, FlowID, CryptFlag},
 	{stop, "Upstream closed.", State};
-relay({flowdata, Data}, {_TrunkEnd, Upstream, _FlowID, _Context}=State) ->
+relay({flowdata, Data}, {_TrunkEnd, Upstream, _FlowID, CryptFlag, _Context}=State) ->
 	ok = gen_tcp:send(Upstream, Data),
 	{next_state, relay, State};
 relay({flowctl, close}, State) ->
@@ -32,7 +34,7 @@ relay(_Msg, State) ->
 	log(log_error, "Ignored unknown message: ~p", [_Msg]),
 	{next_state, relay, State}.
 
-terminate(_Reason, _StateName, {_TrunkEnd, Upstream, _FlowID, _Context}) ->
+terminate(_Reason, _StateName, {_TrunkEnd, Upstream, _FlowID, CryptFlag, _Context}) ->
 	gen_tcp:close(Upstream).
 
 setenv(K, V, Env) ->
