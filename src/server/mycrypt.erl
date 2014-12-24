e8d5b3d961215064965fdbf51f33ee57a58c4441
牛海青 <haiqing1@staff.sina.com.cn>
2014-12-24 14:17:51 +0800

    Added data zip; Fixed SHAREDKEY_BYTESIZE

--- src/server/mycrypt.erl
+++ src/server/mycrypt.erl
@@ -1,27 +1,39 @@
 -module(mycrypt).
 
+-define(SHAREDKEY_BYTESIZE, 16).
+
 -export([decrypt_frame_body/3, decrypt_shared_key/2, encrypt_frame_body/3, encrypt_shared_key/2]).
+-export([rand_sharedkey/0, load_pubkey/1, load_privkey/1]).
 
 -import(log, [log/2, log/3]).
 
+load_pubkey(_Fname) ->
+	<<"Fake_Pub_Key">>.
+
+load_privkey(_Fname) ->
+	<<"Fake_Priv_Key">>.
+
+rand_sharedkey() ->
+	crypto:rand_bytes(?SHAREDKEY_BYTESIZE).
+
 decrypt_frame_body(Data, 0, _SharedKey) ->
 	Data;
-decrypt_frame_body(Data, 1, _SharedKey) ->
-	<<"FAKE_SIMPLE_CRYPT", Plain/binary>>=Data,
-	Plain;
-decrypt_frame_body(Data, 2, _SharedKey) ->
-	<<"FAKE_BLOWFISH_CRYPT", Plain/binary>>=Data,
-	Plain;
+decrypt_frame_body(Data, 1, SharedKey) ->
+	<<"FAKE_SIMPLE_CRYPT_KEY=", SharedKey:?SHAREDKEY_BYTESIZE/binary, Plain/binary>>=Data,
+	zlib:unzip(Plain);
+decrypt_frame_body(Data, 2, SharedKey) ->
+	<<"FAKE_BLOWFISH_CRYPT_KEY=", SharedKey:?SHAREDKEY_BYTESIZE/binary, Plain/binary>>=Data,
+	zlib:unzip(Plain);
 decrypt_frame_body(Data, Type, _SharedKey) ->
 	log(log_debug, "Unsupported encrypt type: ~b\n", [Type]),
 	Data.
 
 encrypt_frame_body(Data, 0, _SharedKey) ->
 	Data;
-encrypt_frame_body(Data, 1, _SharedKey) ->
-    <<"FAKE_SIMPLE_CRYPT", Data/binary>>;
-encrypt_frame_body(Data, 2, _SharedKey) ->
-    <<"FAKE_BLOWFISH_CRYPT", Data/binary>>;
+encrypt_frame_body(Data, 1, SharedKey) ->
+    <<"FAKE_SIMPLE_CRYPT_KEY=", SharedKey:?SHAREDKEY_BYTESIZE/binary, (zlib:zip(Data))/binary>>;
+encrypt_frame_body(Data, 2, SharedKey) ->
+    <<"FAKE_BLOWFISH_CRYPT_KEY=", SharedKey:?SHAREDKEY_BYTESIZE/binary, (zlib:zip(Data))/binary>>;
 encrypt_frame_body(Data, Type, _SharedKey) ->
     log(log_debug, "Unsupported encrypt type: ~b\n", [Type]),
     Data.
