538b593b7458099282337360e20e915e7e048091
牛海青 <haiqing1@staff.sina.com.cn>
2014-12-25 15:11:14 +0800

    Move crypto:start() to correct place.

--- src/server/l7_sup.erl
+++ src/server/l7_sup.erl
@@ -1,6 +1,8 @@
 -module(l7_sup).
 -behaviour(supervisor).
 
+-import(log, [log/2, log/3]).
+
 -export([start_link/0, start_link/1, init/1]).
 
 -define(L7_TEST_CONFIG, [
@@ -14,32 +16,31 @@
     {log_file, "var/log"},
     {log_level, log_info},
 
-    {multiplex_ratio, 5},
-
     {connect_timeout, 2000},
     {recv_timeout, 2000},
 
     {upstreams, [
-        {"10.75.12.0/24",80},
-        {"10.75.12.51",80}]}
+        {{"10.75.12.51",80}, [  {weight, 1},
+                                {max_conns, 1000},
+                                resolve ]},
+        {{"10.75.12.60",80}, [  {weight, 2},
+                                {max_conns, infinite},
+                                resolve]} ]}
 	]).
 
 start_link() -> 
-	io:format("Using default config.\n"),
 	start_link(?L7_TEST_CONFIG).
 
 start_link(L7Config) ->
     supervisor:start_link({local, l7_sup}, l7_sup, L7Config).
 
 init(L7Config) ->
-	io:format("l7_sup:init()\n"),
+	ok = crypto:start(),
 	SockaddrList = [{Addr, Port} || Addr<-config:get(addr, L7Config), Port<-config:get(port, L7Config)],
-	io:format("SockaddrList = ~p\n", [SockaddrList]),
 	ChildList = lists:map(fun (E)->
 		{l7_servername(E), {l7_server, start_link, [E, L7Config]}, permanent, brutal_kill, worker, [l7_server]} end,
 		SockaddrList),
-	io:format("ChildList = ~p\n", [ChildList]),
-	{ok, {{one_for_one, 1000000000000000000000000000000000, 1}, ChildList ++ [
+	{ok, {{one_for_one, 10, 1}, ChildList ++ [
 		{log, {log, start_link, [L7Config]}, permanent, brutal_kill, worker, [log]}
 	]}}.
 
