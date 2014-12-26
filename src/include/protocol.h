7f3a8121be8c7f853fc92d50a2f0bb8014dd9385
牛海青 <haiqing1@staff.sina.com.cn>
2014-12-26 16:20:28 +0800

    Protocol design changed.

--- src/include/protocol.h
+++ src/include/protocol.h
@@ -25,59 +25,66 @@ struct frame_st {
 #define FRAME_CRYPT(X)  ((((X)->frame_flags)&FRAME_FLAG_MASK_ZIP)>>2)
 
 struct frame_body_data_st { 
-	uint32_t msg_type:1;	/* Must == 0 */ 
-	flowid_t flow_id:15; 
+	uint16_t msg_type:1;	/* Must == 0 */ 
+	uint16_t flow_id:15; 
 	uint8_t data[0]; 
 } __attribute__((packed)); 
 
 enum opcode_enum {
-	OP_TRUNK_CONFIG		=0x02,
-	OP_TRUNK_OK			=0x03,
-	OP_TRUNK_FAILURE	=0x04,
+	OP_SOCKET_CERT_REQ	=0x01,
+	OP_SOCKET_CERT		=0x02,
+	OP_SOCKET_KEY_SYNC	=0x03,
+	OP_SOCKET_KEY_REJ	=0x04,
 
-//	OP_FLOW_OPEN	=0x11,
-//	OP_FLOW_FAILURE	=0x12,
-	OP_FLOW_CLOSE	=0x13,
+	OP_PIPELINE_OPEN	=0x11,
+	OP_PIPELINE_CLOSE	=0x13,
 };
 
-struct frame_body_ctl_trunk_config_st {	/* server to client */
-	uint32_t msg_type:1;	/* Must == 1 */
-	uint32_t opcode:15;		/* Must == OP_TRUNK_CONFIG */
+struct frame_body_ctl_socket_cert_req_st {	/* client to server */
+	uint16_t msg_type:1;	/* Must == 1 */
+	uint16_t opcode:15;		/* Must == OP_SOCKET_CERT_REQ */
 	uint8_t x509_certificate[0];
 } __attribute__((packed));
 
-struct frame_body_ctl_trunk_ok_st {		/* client to server */
-	uint32_t msg_type:1;	/* Must == 1 */
-	uint32_t opcode:15;		/* Must == OP_TRUNK_OK */
+struct frame_body_ctl_socket_cert_st {	/* server to client */
+	uint16_t msg_type:1;	/* Must == 1 */
+	uint16_t opcode:15;		/* Must == OP_SOCKET_CERT */
+	uint8_t x509_certificate[0];
+} __attribute__((packed));
+
+struct frame_body_ctl_socket_key_sync_st {		/* client to server */
+	uint16_t msg_type:1;	/* Must == 1 */
+	uint16_t opcode:15;		/* Must == OP_SOCKET_KEY_SYNC */
+	uint32_t crc32;			/* crc32 of shared_key */
 	uint8_t encrypted_shared_key[0];
 } __attribute__((packed));
 
-struct frame_body_ctl_trunk_failure_st {
-	uint32_t msg_type:1;	/* Must == 1 */
-	uint32_t opcode:15;		/* Must == OP_TRUNK_FAILURE */
-	uint8_t error_msg[0];
+struct frame_body_ctl_socket_key_rej_st {		/* server to client */
+	uint16_t msg_type:1;	/* Must == 1 */
+	uint16_t opcode:15;		/* Must == OP_SOCKET_KEY_REJ */
 } __attribute__((packed));
 
-#if 0
-struct frame_body_ctl_flow_open_st {
-	uint32_t msg_type:1;	/* Must == 1 */
-	uint32_t opcode:15;		/* Must == OP_FLOW_OPEN */
-	uint32_t flow_id; 
+struct frame_body_ctl_pipeline_open_st {
+	uint16_t msg_type:1;	/* Must == 1 */
+	uint16_t opcode:15;		/* Must == OP_PIPELINE_OPEN */
+	uint16_t flow_id; 
 	uint16_t max_delay_in_ms;
-	uint8_t request_url[0] ;
+	uint8_t	reply_frame_flags;
+	uint8_t data[0] ;
 } __attribute__((packed)); 
 
+#if 0
 struct frame_body_ctl_flow_failure_st { 
-	uint32_t msg_type:1;	/* Must == 1 */
-	uint32_t opcode:15;		/* Must == OP_FLOW_FAILURE */
-	uint32_t flow_id;
+	uint16_t msg_type:1;	/* Must == 1 */
+	uint16_t opcode:15;		/* Must == OP_FLOW_FAILURE */
+	uint16_t flow_id;
 	uint8_t error_msg[0];
 } __attribute__((packed));
 #endif
 
 struct frame_body_ctl_flow_close_st { 
-	uint32_t msg_type:1;	/* Must == 1 */
-	uint32_t opcode:15;		/* Must == OP__CLOSE */
+	uint16_t msg_type:1;	/* Must == 1 */
+	uint16_t opcode:15;		/* Must == OP__CLOSE */
 	flowid_t flow_id;
 } __attribute__((packed));
 
