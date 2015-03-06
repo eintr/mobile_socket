b4d79dcd1615a0afef5d3a603c51ba43c28b5b6b
牛海青 <haiqing1@staff.sina.com.cn>
2015-03-06 17:01:13 +0800

    Remove dependence of config module.

--- src/client/frame.erl
+++ src/client/frame.erl
@@ -11,10 +11,9 @@ decode(Packet, Context) ->
 encode(Frame, Context) ->
 	encode(v1, Frame, Context).
 
-
 decode(v1, Packet, Context) ->
 	<<1:3, Encrypt:2, Zip:1, _Reserved:2, CryptedFrameBody/binary>> = Packet,
-	FrameBody =  mycrypt:decrypt_frame_body(CryptedFrameBody, Encrypt, Zip, config:get(sharedkey, Context)),
+	FrameBody =  mycrypt:decrypt_frame_body(CryptedFrameBody, Encrypt, Zip, kv:get(sharedkey, Context)),
 	case FrameBody of
 		<<0:1/big-integer, FlowID:15/big-integer, Data/binary>> ->	% Matches data msg
 			{data, FlowID, Encrypt, Zip, Data};
@@ -23,7 +22,7 @@ decode(v1, Packet, Context) ->
 		<<1:1/big-integer, ?OP_SOCKET_CERT:15/big-integer, Certificate/binary>> ->
 			{ctl, socket, cert, {zlib:unzip(Certificate)}};
 		<<1:1/big-integer, ?OP_SOCKET_KEY_SYNC:15/big-integer, CRC32:32/big-integer, SharedKey/binary>> ->
-			{ctl, socket, key_sync, {CRC32, mycrypt:decrypt_shared_key(SharedKey, config:get(privkey, Context))}};
+			{ctl, socket, key_sync, {CRC32, mycrypt:decrypt_shared_key(SharedKey, kv:get(privkey, Context))}};
 		<<1:1/big-integer, ?OP_SOCKET_KEY_REJ:15/big-integer, _/binary>> ->
 			{ctl, socket, key_rej, {}};
 		<<1:1/big-integer, ?OP_PIPELINE_OPEN:15/big-integer, FlowID:16/big-integer, MaxDelay:16/big-integer, ReplyFLag:8/big-integer, Data/binary>> ->
@@ -40,7 +39,7 @@ encode(v1, {data, FlowID, CryptFlag, Zip, RawData}, Context) ->
 	RawBody = <<	0:1/big-integer,
 					FlowID:15/big-integer,
 					RawData/binary >>,
-	CryptedBody =  mycrypt:encrypt_frame_body(RawBody, CryptFlag, Zip, config:get(sharedkey, Context)),
+	CryptedBody =  mycrypt:encrypt_frame_body(RawBody, CryptFlag, Zip, kv:get(sharedkey, Context)),
 	{ok, <<1:3/big-integer, CryptFlag:2/big-integer, Zip:1/big-integer, 0:2/big-integer, CryptedBody/binary>>};
 encode(v1, {ctl, socket, cert_req, _}, _Context) ->
 	Frame_body = <<	1:1/big-integer,
@@ -68,11 +67,11 @@ encode(v1, {ctl, pipeline, open, {FlowID, CryptFlag, Zip, MaxDelay, ReplyFlags, 
 					MaxDelay:16/big-integer,
 					ReplyFlags:8/big-integer,
 					Data/binary>>,
-	Frame_body = mycrypt:encrypt_frame_body(Rawbody, CryptFlag, Zip, config:get(sharedkey, Context)),
+	Frame_body = mycrypt:encrypt_frame_body(Rawbody, CryptFlag, Zip, kv:get(sharedkey, Context)),
 	{ok, <<1:3/big-integer, CryptFlag:2/big-integer, Zip:1/big-integer, 0:2/big-integer, Frame_body/binary>>};
 encode(v1, {ctl, pipeline, close, {FlowID, CryptFlag, Zip}}, Context) ->
 	Raw_body = <<1:1/big-integer, ?OP_PIPELINE_CLOSE:15/big-integer, FlowID:16/big-integer>>,
-	Frame_body = mycrypt:encrypt_frame_body(Raw_body, CryptFlag, Zip, config:get(sharedkey, Context)),
+	Frame_body = mycrypt:encrypt_frame_body(Raw_body, CryptFlag, Zip, kv:get(sharedkey, Context)),
 	{ok, <<1:3/big-integer, CryptFlag:2/big-integer, Zip:1/big-integer, 0:2/big-integer, Frame_body/binary>>};
 
 % TODO: to be continued.
